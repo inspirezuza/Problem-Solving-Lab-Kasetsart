1b00c072d57d21bdf5f62e3dc84a4ebdb6f8841d
inspirezuza <inspire7220@gmail.com>
2023-11-28 20:40:55 +0700

    first commit

---
+++ week1_cpp/abcd-012113.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << a+b+c+d;
+
+    return 0;
+}
